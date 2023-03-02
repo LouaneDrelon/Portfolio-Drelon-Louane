3633bdb1f6614c4a4361b8ca844e6d8e8158ea92
LouaneDrelon <115797867+LouaneDrelon@users.noreply.github.com>
2023-03-02 21:41:52 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const dragon = document.querySelector('#dragon')
+const wolf = document.querySelector('#wolf')
+const profil = document.querySelector('profil')
+
+
+window.addEventListener('load', () => {
+    
+    const TL = gsap.timeline({paused : true}) ;
+
+    TL
+    .from(wolf, 5 ,{left : 300, opacity : 0, ease : 'power2.out'})
+    .from(dragon, 5,{right: 300, opacity: 0, ease : 'power2.out'}, '-=5')
+    .fromTo("#profil", 5 ,{scale: 0.7, opacity:0},{scale : 1,opacity: 0.8, ease : 'power2'}, '-=5')
+    
+    TL.play()
+})
+
